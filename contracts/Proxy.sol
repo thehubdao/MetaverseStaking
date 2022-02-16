4aa8e8d7e93eb740a143ab5a03f4ac23816e0b95
DAC-PhilThomsen <phil.thomsen@thedac.info>
2022-02-16 15:10:13 +0000

    custom proxy implementation (upgrader, appointNerUpgrader())

--- contracts/Proxy.sol
+++ contracts/Proxy.sol
@@ -4,6 +4,55 @@ pragma solidity ^0.8.1;
 import "@openzeppelin/contracts/proxy/Proxy.sol";
 
 
-contract StakingProxy {
-    
+contract MVSProxy is Proxy {
+
+    bytes32 constant _SLOT_ = 0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc;
+
+    struct ProxyStorage {
+        address implementation;
+        address upgrader;
+    }
+
+    constructor(address implementation_, address _upgrader, bytes memory data) {
+        _getProxyStorage().upgrader = msg.sender;
+        upgradeToAndCall(implementation_, data);
+        appointNewUpgrader(_upgrader);
+    }
+
+    function _implementation() internal view override returns(address) {
+        return _getProxyStorage().implementation;
+    }
+
+    function _getProxyStorage() internal pure returns(ProxyStorage storage s) {
+        bytes32 _slot = _SLOT_;
+        assembly {
+            s.slot := _slot
+        }
+    }
+
+    function upgradeToAndCall(address newImplementation, bytes memory data) public {
+        ProxyStorage storage s = _getProxyStorage();
+
+        require(msg.sender == s.upgrader, "not the upgrader");
+        require(_isContract(newImplementation), "not a contract");
+
+        s.implementation = newImplementation;
+        if(data.length != 0) {
+            (bool success,) = newImplementation.delegatecall(data);
+            if(!success) revert("delegatecall failed");
+        }
+    }
+
+    function appointNewUpgrader(address newUpgrader) public {
+        require(msg.sender == _getProxyStorage().upgrader);
+        _getProxyStorage().upgrader = newUpgrader;
+    }
+
+    function _isContract(address newImplementation) internal view returns(bool) {
+        uint256 size;
+        assembly {
+            size := extcodesize(newImplementation)
+        }
+        return size > 0;
+    }
 }
